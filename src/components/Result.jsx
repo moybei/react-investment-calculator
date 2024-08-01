1b54e1ebaebbfc00c175842e727751c8fc9f12a5
Moy Haobei <haobei.moy@vitrox.com>
2024-08-01 11:17:10 +0800

    - Added UserInput area - Added Result calculation

---
+++ src/components/Result.jsx
@@ -0,0 +1,43 @@
+import { calculateInvestmentResults, formatter } from "../util/investment";
+
+export default function Result({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].annualInvestment -
+    resultsData[0].interest;
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((data) => {
+          const totalInterest =
+            data.valueEndOfYear -
+            data.annualInvestment * data.year -
+            initialInvestment;
+
+          const totalAmountInvested = data.valueEndOfYear - totalInterest;
+
+          return (
+            <tr key={data.year}>
+              <td>{data.year}</td>
+              <td>{formatter.format(data.valueEndOfYear)}</td>
+              <td>{formatter.format(data.interest)}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
